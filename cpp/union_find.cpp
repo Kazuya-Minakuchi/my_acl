ea7d55f7e1833bfe2156a595fdfa8646366a69de
Kazuya <kazuya.minakuchi12@gmail.com>
2024-02-15 23:57:19 +0900

    Remove duplicate code

--- cpp/union_find.cpp
+++ cpp/union_find.cpp
@@ -4,11 +4,6 @@ using namespace std;
 #define all(v) v.begin(), v.end()
 
 
-#include <bits/stdc++.h>
-using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define all(v) v.begin(), v.end()
-
 class UnionFind {
   public:
   vector<int> par;
