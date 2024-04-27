541f4e6e145c39a75bdfcee50e1cc99361d83923
Kazuya <kazuya.minakuchi12@gmail.com>
2024-04-27 16:26:10 +0900

    Use macro

--- cpp/dijkstra.cpp
+++ cpp/dijkstra.cpp
@@ -1,16 +1,43 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
+
+typedef int64_t ll;
+const int INF = 1e9;
+const int MOD = 1e9+7;
+const ll LINF = 1e18;
+const ll LZERO = 0;
+
 using namespace std;
+using mint = atcoder::modint998244353;
+
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define reps(i, n) for(int i = 1; i <=(int)(n); i++)
 #define all(v) v.begin(), v.end()
+#define pb push_back
+#define SZ(x) ((int)(x).size())
+#define dump(x) cerr << #x << " = " << (x) << endl;
+#define YES(n) cout << ((n) ? "YES" : "NO"  ) << endl
+#define Yes(n) cout << ((n) ? "Yes" : "No"  ) << endl
+#define COUT(x) cout<<(x)<<endl
+#define CERR(x) cerr<<(x)<<endl
+#define SCOUT(x) cout<<(x)<<" "
+#define SCERR(x) cerr<<(x)<<" "
+#define ENDL cout<<endl
+#define VECCOUT(x) for(auto&youso_: (x) )cout<<youso_<<" ";cout<<endl
+#define VECCERR(x) for(auto&youso_: (x) )cerr<<youso_<<" ";cerr<<endl
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
 
-int64_t INF = 1e18;
+ll D, H, K, L, M, N, Q, R, W;
+string S;
+char C;
 
-vector<int64_t> get_cost_dijk(int start, vector<vector<pair<int, int64_t>>> &G) {
+vector<ll> get_cost_dijk(int start, vector<vector<pair<int, ll>>> &G) {
   // set variables
   int n = G.size();
-  vector<int64_t> cost(n + 1, INF);
+  vector<ll> cost(n + 1, LINF);
   vector<bool> kakutei(n + 1, false);
-  priority_queue<pair<int64_t, int>, vector<pair<int64_t, int>>, greater<pair<int64_t, int>>> Q;
+  priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> Q;
   // init
   cost.at(start) = 0;
   Q.push(make_pair(cost[start], start));
@@ -22,7 +49,7 @@ vector<int64_t> get_cost_dijk(int start, vector<vector<pair<int, int64_t>>> &G) 
     kakutei.at(pos) = true;
     for (auto p : G.at(pos)) {
       int nex = p.first;
-      int64_t cst = p.second;
+      ll cst = p.second;
       if (cost.at(nex) > cost.at(pos) + cst) {
         cost.at(nex) = cost.at(pos) + cst;
         Q.push(make_pair(cost.at(nex), nex));
@@ -34,35 +61,31 @@ vector<int64_t> get_cost_dijk(int start, vector<vector<pair<int, int64_t>>> &G) 
 
 int main() {
   // ABC325 E
-  int N;
-  int64_t A, B, C;
+  ll A, B, C;
   cin >> N >> A >> B >> C;
+  vector<vector<pair<int, ll>>> G_c(N + 1), G_t(N + 1);
 
-  vector<vector<pair<int, int64_t>>> G_c(N + 1), G_t(N + 1);
-
-  for (int i = 1; i <= N; i++){
-    for (int j = 1; j <= N; j++){
-      int64_t d;
+  reps(i, N) {
+    reps(j, N) {
+      ll d;
       cin >> d;
-      G_c.at(i).push_back(make_pair(j, d * A));
-      G_t.at(i).push_back(make_pair(j, d * B + C));
+      G_c.at(i).pb(make_pair(j, d * A));
+      G_t.at(i).pb(make_pair(j, d * B + C));
     }
   }
 
-  vector<int64_t> cost_c = get_cost_dijk(1, G_c);
-  vector<int64_t> cost_t = get_cost_dijk(N, G_t);
+  vector<ll> cost_c = get_cost_dijk(1, G_c);
+  vector<ll> cost_t = get_cost_dijk(N, G_t);
 
-  int64_t ans = INF;
-  for (int i = 1; i <= N; i++) ans = min(ans, cost_c.at(i) + cost_t.at(i));
-  cout << ans << endl;
+  ll ans = LINF;
+  reps(i, N) chmin(ans, cost_c.at(i) + cost_t.at(i));
+  COUT(ans);
 
   cerr << endl;
-  cerr << N << endl;
-  cerr << A << endl;
-  cerr << B << endl;
-  cerr << C << endl;
-  // for (int i = 1; i <= N; i++) cerr << cost_c.at(i) << ' ';
-  // cerr << endl;
-  // for (int i = 1; i <= N; i++) cerr << cost_t.at(i) << ' ';
-  // cerr << endl;
+  dump(N);
+  dump(A);
+  dump(B);
+  dump(C);
+  // VECCERR(cost_c);
+  // VECCERR(cost_t);
 }
